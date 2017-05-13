0fb7ff3d47fc4436f293ae9f7ebd2317fb09f021
SteveLin <fudanxiaoan@163.com>
2017-05-13 19:29:13 +0800

    Creat basic project and  parse input

--- Package.swift
+++ Package.swift
@@ -3,5 +3,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "fengniao"
+    name: "fengniao",
+    dependencies: [.Package(url:"https://github.com/jatoben/CommandLine","3.0.0-pre1"),
+                   .Package(url:"https://github.com/onevcat/Rainbow","2.0.1")]
 )
+
+//import PackageDescription // 官方模版
+//
+//let package = Package(
+//    name: "DeckOfPlayingCards",
+//    targets: [],
+//    dependencies: [
+//        .Package(url: "https://github.com/apple/example-package-fisheryates.git",
+//                 majorVersion: 1),
+//        .Package(url: "https://github.com/apple/example-package-playingcard.git",
+//                 majorVersion: 1),
+//        ]
+//)
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,67 @@
-print("Hello, world!")
+
+import Foundation
+import CommandLineKit
+import Rainbow
+
+let cli = CommandLineKit.CommandLine()
+
+let projectOption = StringOption(shortFlag: "p", longFlag: "project",
+                            helpMessage: "Path to the project.")
+let excludePathsOption = MultiStringOption(shortFlag: "e", longFlag: "exclude", helpMessage: "Exclude paths which should not search in.")
+let resourceExtensionsOption = MultiStringOption(shortFlag: "r", longFlag: "resource-extensions", helpMessage: "Extension to search")
+let fileExtensionsOption = MultiStringOption(shortFlag: "f", longFlag: "file-extensions", helpMessage: "file Extension to search with.")
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+cli.addOptions(projectOption, resourceExtensionsOption,fileExtensionsOption, help)
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.blue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s: str, type: type)
+}
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if help.value {
+    cli.printUsage()
+    exit(EX_OK)
+}
+
+let project = projectOption.value ?? "."
+let resourceExtensions = resourceExtensionsOption.value ?? ["png","jpg","imageset"]
+let fileExtentions = fileExtensionsOption.value ?? ["swift","m","mm","xib","storyboard"]
+let excudePaths = excludePathsOption.value ?? [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
